8e1525d98840a2fa7323a2128f29019b801e176f
astya965 <astya965@gmail.com>
2023-04-09 20:09:43 +0200

    feat: Add html plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app";
 
-ReactDOM.render(
-    <App />, 
-    document.getElementById('app')
-);
+createRoot(document.getElementById("app")).render(<App />);
